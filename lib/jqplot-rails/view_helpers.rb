5e5ba160133830a5eecab05666d8a9989b10ebe2
Walter Brebels <walter.brebels@gmail.com>
2017-04-18 22:48:18 +0200

    Ruby 1.8.7 compatibility

--- lib/jqplot-rails/view_helpers.rb
+++ lib/jqplot-rails/view_helpers.rb
@@ -4,10 +4,10 @@ module Jqplot::Rails
       dom_id = SecureRandom.hex(12)
       height = height.is_a?(String) ? height : "#{height}px"
       width = width.is_a?(String) ? width : "#{width}px"
-      data_hash = {series: data_series, options: options, role: 'jqplot'}
-      data_hash.merge!({name: data_name}) if data_name
-      content_tag :div, nil, data: data_hash,
-        id: dom_id, style: "height:#{height};width:#{width}"
+      data_hash = {:series => data_series, :options => options, :role => 'jqplot'}
+      data_hash.merge!({:name => data_name}) if data_name
+      content_tag :div, nil, :data => data_hash,
+        :id => dom_id, :style => "height:#{height};width:#{width}"
     end
   end
 end
